e8d9a6eb5846ae9183cb19318b969ad85d982df8
biqin <bq7327645@163.com>
2020-10-12 21:33:20 +0800

    dd
    dd

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallTinyApplication {
 
     public static void main(String[] args) {
+        system.out.println("haha");
         SpringApplication.run(MallTinyApplication.class, args);
     }
 
