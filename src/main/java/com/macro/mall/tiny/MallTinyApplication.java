c750c52417c0195f70ff63f18774586c4624d9f6
bq <bq7327645@163.com>
2020-11-23 20:52:48 +0800

    es

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -3,6 +3,8 @@ package com.macro.mall.tiny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @SpringBootApplication
 public class MallTinyApplication {
 
